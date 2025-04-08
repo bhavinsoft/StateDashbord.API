85e07c2e7ec5930ffb04fb58e410c6b118eacd0d
bhavin patel <bhavinpatelsoft@gmail.com>
2025-04-08 23:20:08 +0530

    dd

---
+++ StateDashbord.Domain/Entities/FridataList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateDashbord.Domain.Entities
+{
+    public class FridataList
+    {
+        public int recid { get; set; }
+        public string? ps_cd { get; set; }
+        public string? ps_name { get; set; }
+        public string? fir_reg_num { get; set; }
+        public string? reg_dt { get; set; }
+        public string? city_district_name { get; set; }
+        public string? crimehead_desc_guj { get; set; }
+
+    }
+}
